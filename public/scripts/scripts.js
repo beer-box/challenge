7635a132ee307733a407f7f177d7d4e6a5662698
Gytis Talocka <gytistalocka@Gytiss-MacBook-Pro.local>
2018-07-27 21:09:53 +0300

    countdown and banner

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,26 +1,58 @@
-(function(){
-  $(document).ready(loadTemplate);
+var BEGIN_TIME = new Date('2018-07-28T11:00:00.000Z');
+var interval;
+var countdown = $('#countdown');;
 
-  function loadTemplate() {
-    var hash = getUrlParameter('hash');
+$(document).ready(loadTemplate);
 
-    if (hash) {
-      $('#task').load('templates/' + hash + '.html');
-    }
+function loadTemplate() {
+  var hash = getUrlParameter('hash');
+
+  if (hash && timeDiff() < 0) {
+    $('section').show();
+    $('#task').load('templates/' + hash + '.html');
+    return;
   }
 
-  function getUrlParameter(sParam) {
-    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
-      sURLVariables = sPageURL.split('&'),
-      sParameterName,
-      i;
+  countdownCheck();
+  interval = setInterval(countdownCheck, 1000);
+}
 
-    for (i = 0; i < sURLVariables.length; i++) {
-      sParameterName = sURLVariables[i].split('=');
+function countdownCheck() {
+  if (!timeDiff()) {
+    clearInterval(interval);
+    countdown.remove();
+    $('section').show();
+  }
 
-      if (sParameterName[0] === sParam) {
-        return sParameterName[1] === undefined ? true : sParameterName[1];
-      }
+  renderTimeDiff(timeDiff());
+}
+
+function renderTimeDiff(diff) {
+  var h = diff / 3600 >> 0
+  var m = diff % 3600 / 60 >> 0
+  var s = diff % 60;
+  countdown.text(`Come back later! Challenge starts in: ${pad(h)}:${pad(m)}:${pad(s)}`);
+}
+
+function pad(number) {
+  return ('0000' + number).slice(-2);
+}
+
+function getUrlParameter(sParam) {
+  var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+    sURLVariables = sPageURL.split('&'),
+    sParameterName,
+    i;
+
+  for (i = 0; i < sURLVariables.length; i++) {
+    sParameterName = sURLVariables[i].split('=');
+
+    if (sParameterName[0] === sParam) {
+      return sParameterName[1] === undefined ? true : sParameterName[1];
     }
   }
-})();
+}
+
+function timeDiff() {
+  return Math.floor((BEGIN_TIME - new Date()) / 1000);
+}
